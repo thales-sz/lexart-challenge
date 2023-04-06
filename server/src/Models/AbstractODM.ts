68a23414fcb49b006cd6ccc61c6b952aab533097
Thales <thales.souz@outlook.com>
2023-04-06 10:47:33 -0300

    feat(back): created abstractODM for mongoose models

---
+++ server/src/Models/AbstractODM.ts
@@ -0,0 +1,36 @@
+import type { Model, Schema } from 'mongoose'
+import { model, models } from 'mongoose'
+
+abstract class AbstractODM<T> {
+  protected model: Model<T>
+  protected schema: Schema
+  protected modelName: string
+
+  constructor (schema: Schema, modelName: string) {
+    this.schema = schema
+    this.modelName = modelName
+    this.model = models[this.modelName] || model(this.modelName, this.schema)
+  }
+
+  public async create (obj: T): Promise<T> {
+    return await this.model.create({ ...obj })
+  }
+
+  public async get (): Promise<T[]> {
+    return await this.model.find()
+  }
+
+  public async getById (id: string): Promise<T | null> {
+    return await this.model.findById(id)
+  }
+
+  public async update (id: string, obj: Partial<T>): Promise<T | null> {
+    return await this.model.findByIdAndUpdate(
+      { _id: id },
+      { ...obj },
+      { new: true }
+    )
+  }
+}
+
+export default AbstractODM
