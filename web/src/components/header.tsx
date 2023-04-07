5fba4716739e63df6f563934a66edce31e336593
Thales <thales.souz@outlook.com>
2023-04-07 15:44:33 -0300

    feat(front): dropdown component and config eslint

---
+++ web/src/components/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DropDown from './dropdown'
+
+function Header (): JSX.Element {
+  return (
+    <div>
+      <DropDown />
+    </div>
+  )
+}
+
+export default Header
