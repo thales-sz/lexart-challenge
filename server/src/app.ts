84fb5ab2df0d367a49aa9ccd0b817c33474cb271
Thales <thales.souz@outlook.com>
2023-04-08 09:57:20 -0300

    feat(back): deploy

--- server/src/app.ts
+++ server/src/app.ts
@@ -25,7 +25,7 @@ class App {
     }
 
     const limiter = rateLimit({
-      windowMs: 15 * 60 * 1000, // 15 minutes
+      windowMs: 15 * 60 * 1000,
       max: 100,
       standardHeaders: true,
       legacyHeaders: false,
