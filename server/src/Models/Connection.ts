139917bdb481ab10bc0eba493f1e361955046a15
Thales <thales.souz@outlook.com>
2023-04-08 09:34:24 -0300

    feat(back): adding env variables

--- server/src/Models/Connection.ts
+++ server/src/Models/Connection.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import 'dotenv/config'
 
-const { MONGO_URL } = process.env
+const MONGO_URL = process.env.MONGO_URL ?? 'mongodb+srv://dev:H4XubxdHDz9wfAkX@lexart-db.6iqadqd.mongodb.net/?retryWrites=true&w=majority'
 
 const connectToDatabase = async (
   mongoDatabaseURI = MONGO_URL
