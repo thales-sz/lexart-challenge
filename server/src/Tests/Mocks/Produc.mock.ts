398e843a8390c433d1b9e337c14e6a90563154ca
Thales <thales.souz@outlook.com>
2023-04-08 13:40:14 -0300

    fix(back): enum for Web removed

--- server/src/Tests/Mocks/Produc.mock.ts
+++ server/src/Tests/Mocks/Produc.mock.ts
@@ -1,10 +1,9 @@
 import type IProduct from '../../Domain/Interfaces/Products.interface'
-import { Web } from '../../Domain/Interfaces/Products.interface'
 
 export const productMock: IProduct = {
   category: 'Product',
   description: 'Product description',
   price: 780,
-  web: Web.mercado_livre,
+  web: 'https://api.mercadolibre.com/sites/MLB/search?q=',
   image: 'imagem bela'
 }
